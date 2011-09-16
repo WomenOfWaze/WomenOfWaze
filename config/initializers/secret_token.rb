c7f30181649c4c42481a24625c99d77fd49dca01
Pat Shaughnessy <pat@patshaughnessy.net>
2011-09-16 06:57:56 -0400

    New Rails 3.1 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WomenOfWaze::Application.config.secret_token = '8efe0b06e69d617853e4fd0447d7364ecd9a1f1370ec7ab0690d26b9210308bc3c9ab4515859fe73267f860bce6744025e5665b42f8369f70d5d0fc60d626c57'
