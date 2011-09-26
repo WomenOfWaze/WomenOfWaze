6b140fc9cdf00e3882450a0ff37e68d9fd133e1f
Arun Agrawal <arunagw@gmail.com>
2011-09-26 22:50:42 +0530

    Adding travis.yml file to travis-ci.org

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,15 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+if ENV['TRAVIS']
+  configs                             = YAML.load_file('test/database.yml')
+  ActiveRecord::Base.configurations   = configs
+
+  db_name                             = ENV['DB'] || 'sqlite'
+  ActiveRecord::Base.establish_connection(db_name)
+  ActiveRecord::Base.default_timezone = :utc
+end
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
   #
