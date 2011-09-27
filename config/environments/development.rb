cc0ed93d59c8a86f8f884bced580d12b3f5416fa
ankit gupta <ankit.gupta8898@gmail.com>
2011-09-27 17:35:39 +0530

    deleted:    public/assets/manifest.yml

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,5 +26,6 @@ WomenOfWaze::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
+
 end
