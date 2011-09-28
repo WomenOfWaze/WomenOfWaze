033529b06483f424b949332efc23b11cec0c93a6
ankit gupta <ankit.gupta8898@gmail.com>
2011-09-28 16:50:29 +0530

    modified:   app/helpers/products_helper.rb 	modified:   app/views/products/catalogue.html.erb

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,13 @@
 module ProductsHelper
+  def formatted_products products
+    formatted = []
+    b = []
+    while !products.empty?
+      b = products.take(4)
+      formatted << b
+      products = products - b
+    end
+    formatted
+  end
+  
 end
