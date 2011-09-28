b8d3890443cdc6c86fef9317ac37ef8ab6730b33
doel sengupta <doels11@gmail.com>
2011-09-28 13:16:13 +0530

    changed text to string datatype for remarks

--- db/migrate/20110926154658_create_enquiries.rb
+++ db/migrate/20110926154658_create_enquiries.rb
@@ -8,7 +8,7 @@ class CreateEnquiries < ActiveRecord::Migration
       t.integer :phone_city_code, :limit => 3
       t.integer :phone_number, :limit => 11
       t.string :email, :null => false, :limit => 65
-      t.text :remarks, :null => false, :limit => 256
+      t.string :remarks, :null => false, :limit => 256
 
       t.timestamps
     end
