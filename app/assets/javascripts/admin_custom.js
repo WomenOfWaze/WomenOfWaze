9947121fb1e5074de61f27f737524c419c662959
Ankur Gera <ankurgera@gmail.com>
2011-10-17 18:26:19 +0530

    Admin UI final changes done

--- app/assets/javascripts/admin_custom.js
+++ app/assets/javascripts/admin_custom.js
@@ -53,36 +53,36 @@ $(function(){
     },
     messages: {
         "category[name]": {
-            required: "&nbsp;&nbsp;Category name is required",
-            maxlength: "&nbsp;&nbsp;Category name should not be more than 64 characters"
+            required: "Category name is required",
+            maxlength: "Category name should not be more than 64 characters"
         },
         "category[description]": {
-            required: "&nbsp;&nbsp;Category description is required",
-            maxlength: "&nbsp;&nbsp;Category description should not be more than 64 characters"
+            required: "Category description is required",
+            maxlength: "Category description should not be more than 64 characters"
         },
         "sub_category[name]": {
-            required: "&nbsp;&nbsp;SubCategory name is required",
-            maxlength: "&nbsp;&nbsp;SubCategory name should not be more than 64 characters"
+            required: "SubCategory name is required",
+            maxlength: "SubCategory name should not be more than 64 characters"
         },
         "sub_category[description]": {
-            required: "&nbsp;&nbsp;SubCategory description is required",
-            maxlength: "&nbsp;&nbsp;SubCategory description should not be more than 64 characters"
+            required: "SubCategory description is required",
+            maxlength: "SubCategory description should not be more than 64 characters"
         },
          "product[name]": {
-            required: "&nbsp;&nbsp;Product name is required",
-            maxlength: "&nbsp;&nbsp;Product Name should not be more than 64 characters"
+            required: "Product name is required",
+            maxlength: "Product Name should not be more than 64 characters"
         },
         "product[description]": {
-            required: "&nbsp;&nbsp;Product description is required",
-            maxlength: "&nbsp;&nbsp;Product description should not be more than 256 characters"
+            required: "Product description is required",
+            maxlength: "Product description should not be more than 256 characters"
         },
         "product[price]": {
-            required: "&nbsp;&nbsp;Product price is required",
-            maxlength: "&nbsp;&nbsp;Product price should not be more than 10 characters"
+            required: "Product price is required",
+            maxlength: "Product price should not be more than 10 characters"
         },
         "product[size]": {
-            required: "&nbsp;&nbsp;Product size is required",
-            maxlength: "&nbsp;&nbsp;Product size should not be more than 15 characters"
+            required: "Product size is required",
+            maxlength: "Product size should not be more than 15 characters"
         }
     }
  });
